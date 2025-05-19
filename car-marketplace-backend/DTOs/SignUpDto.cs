dc9540f37aa20a4daa094f72ea1213e6e1b2831e
Berco Andrei <andrei2003berco@gmail.com>
2025-05-19 12:25:24 +0300

    Add DTO's for login and signup

---
+++ car-marketplace-backend/DTOs/SignUpDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace car_marketplace_backend.DTOs
+{
+    public class SignUpDto
+    {
+        [Required][StringLength(50)] 
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        public string? Role { get; set; }
+    }
+}
---
+++ car-marketplace-backend/DTOs/TokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace car_marketplace_backend.DTOs
+{
+    public class TokenRequestDto
+    {
+        [Required] 
+        public string Role { get; set; } = string.Empty;
+    }
+}
