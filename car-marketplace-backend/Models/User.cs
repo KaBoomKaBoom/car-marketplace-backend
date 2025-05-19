7d6d8e8b7e07332456cffe1998748b95b53cf342
Berco Andrei <andrei2003berco@gmail.com>
2025-05-19 12:22:13 +0300

    Add model for user

---
+++ car-marketplace-backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace car_marketplace_backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty; 
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Role { get; set; } = "USER";
+    }
+}
+
