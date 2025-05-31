e41f55b3645f21a3ece69379e5b25dccbe8095f1
Berco Andrei <andrei2003berco@gmail.com>
2025-05-31 15:27:39 +0300

    Add endpoint to create a new offer

--- car-marketplace-backend/Controllers/userController.cs
+++ car-marketplace-backend/Controllers/userController.cs
@@ -1,6 +1,8 @@
 namespace car_marketplace_backend.Controllers
 {
     using car_marketplace_backend.Data;
+    using car_marketplace_backend.DTOs;
+    using car_marketplace_backend.Models;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
@@ -47,6 +49,51 @@ namespace car_marketplace_backend.Controllers
                     return NotFound($"Car with ID {id} not found.");
                 }
                 return Ok(car);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Database error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("addCar")]
+        [Authorize(Roles = "USER")]
+        public async Task<IActionResult> AddCar([FromBody] AddCarDto addCarDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var car = new Car
+                {
+                    UserId = addCarDto.UserId,
+                    Make = addCarDto.Make,
+                    Model = addCarDto.Model,
+                    Year = addCarDto.Year,
+                    Price = addCarDto.Price,
+                    Mileage = addCarDto.Mileage,
+                    Type = addCarDto.Type,
+                    Condition = addCarDto.Condition,
+                    Category = addCarDto.Category,
+                    Fuel = addCarDto.Fuel,
+                    ImageUrl = addCarDto.ImageUrl,
+                    HorsePower = addCarDto.HorsePower,
+                    Torque = addCarDto.Torque,
+                    Transmission = addCarDto.Transmission,
+                    Color = addCarDto.Color,
+                    Interior = addCarDto.Interior,
+                    Drive = addCarDto.Drive,
+                    Description = addCarDto.Description,
+                    CylinderCapacity = addCarDto.CylinderCapacity,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                _context.Cars.Add(car);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetCarById), new { id = car.Id }, car);
             }
             catch (Exception ex)
             {
